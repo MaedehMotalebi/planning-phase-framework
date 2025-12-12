6c8a75ba649eb84151df9fdca11af5d55f75522e
MaedehMotalebi <128955136+MaedehMotalebi@users.noreply.github.com>
2025-12-12 13:06:59 +1100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 import "./index.css";
-import PlanningPhase_Animated_Numbered from "./PlanningPhase_Animated_Numbered.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <PlanningPhase_Animated_Numbered />
+    <App />
   </React.StrictMode>
 );
