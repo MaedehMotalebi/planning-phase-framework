a2e19d6cd611992e2c206729858e5d842cce08fb
MaedehMotalebi <128955136+MaedehMotalebi@users.noreply.github.com>
2025-12-12 12:58:30 +1100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import PhaseFlow_Animated_Numbered from "./PlanningPhase_Animated_Numbered";
+import PhaseFlow_Animated_Numbered from "./PhaseFlow_Animated_Numbered";
 
 export default function App() {
   return <PhaseFlow_Animated_Numbered />;
