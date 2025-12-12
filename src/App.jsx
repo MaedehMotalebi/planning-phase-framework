13fc5757e9aa54f1ff4dc07bae63370a7e2dd1c5
MaedehMotalebi <128955136+MaedehMotalebi@users.noreply.github.com>
2025-12-12 13:20:51 +1100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,12 @@ import React from "react";
 import PlanningPhase_Animated_Numbered from "./PlanningPhase_Animated_Numbered";
 
 function App() {
-  return <PlanningPhase_Animated_Numbered />;
+  return (
+    <div>
+      <PlanningPhase_Animated_Numbered />
+    </div>
+  );
 }
 
 export default App;
+
